3657cb9a528cb9c3e0dbc0d6d68f0887298fa91a
LunaticStark <HyrimStark@gmail.com>
2026-05-19 10:56:54 -0400

    Changed some stuff
    aaaaaaaaaaaaaaaaaaaaa!

--- script.js
+++ script.js
@@ -14,3 +14,44 @@ document.addEventListener("click", (event) => {
         expandedMenu.style.display = "none";
     }
 });
+
+const slider = document.getElementById("content-container");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("dragstart", (e) => {
+    e.preventDefault();
+});
+
+slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+}); 
+
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+});
+
+slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.8; 
+
+    let targetScrollLeft = scrollLeft - walk;
+    const maxScroll = (slider.scrollWidth - slider.clientWidth) - 1;
+    if (targetScrollLeft < 0) {
+        targetScrollLeft = 0;
+    } else if (targetScrollLeft > maxScroll) {
+        targetScrollLeft = maxScroll;
+    }
+    slider.scrollLeft = targetScrollLeft;
+});
+
+
