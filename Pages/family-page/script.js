f560abc395feeb665f1cb9facc590befbde83a1b
LunaticStark <HyrimStark@gmail.com>
2026-06-13 16:17:30 -0400

    changed stuff
    help me I have been coding for hours!!!!!!

--- Pages/family-page/script.js
+++ Pages/family-page/script.js
@@ -12,10 +12,69 @@ const familyMembers  = [
     {
         name: "Ammon & Jeanelle Stark",
         familyphoto: "../../Art-stuff/family-photos/Ammon&jeanelleStark.png",
+        photobuttons: [
+            {
+                name: "Stonewall",
+                top: "48%",
+                left: "85%",
+                width: "35px",
+                height: "40px",
+            },
+            {
+                name: "Truck",
+                top: "55%",
+                left: "35%",
+                width: "35px",
+                height: "60px",
+            },
+            {
+                name: "Hyrim",
+                top: "30%",
+                left: "42%",
+                width: "35px",
+                height: "50px",
+            },
+            {
+                name: "Shepherd",
+                top: "55%",
+                left: "76%",
+                width: "30px",
+                height: "60px",
+            },
+            {
+                name: "Mark & Robin Hannon",
+                top: "50%",
+                left: "49%",
+                width: "40px",
+                height: "60px",
+            },
+            {
+                name: "Mark & Robin Hannon",
+                top: "53%",
+                left: "60%",
+                width: "40px",
+                height: "60px",
+            },
+        ],
         descriprion: "",
         children: ["Stonewall, Truck, Hyrim, Shepherd"],
         parents1: ["Stanley Stark, Dorothy Stark"],
         parents2: ["Mark Hannon, Robin Hannon"],
+    },
+    {
+        name: "Stonewall",
+    },
+    {
+        name: "Truck",
+    },
+    {
+        name: "Hyrim",
+    },
+    {
+        name: "Shepherd",
+    },
+    {
+        name: "Mark & Robin Hannon"
     },
     {
         name: "Ben",
@@ -54,29 +113,67 @@ const familyMembers  = [
         name: "Cindy",
         familyphoto: "../../Art-stuff/family-photos/Cindy.png",
     },
-]
+];
+const navhistory = [];
 
 function test(item) {
+
     const testH1 = document.getElementById('testH1');
     const logo = document.getElementById('logo');
-    const image = document.getElementById('testImg');
+    const image = document.getElementById('displayImg');
+    const imgContainer = document.getElementById('Imgcontainer')
+    const oldbuttons = imgContainer.querySelectorAll('.family-button');
+    oldbuttons.forEach(oldBtn => oldBtn.remove());
     const description = document.getElementById('description')
     const childrenList = document.getElementById('children')
     const parentsList1 = document.getElementById('parent1')
     const parentsList2 = document.getElementById('parent2')
     testH1.innerText = item.name;
     logo.innerText = item.name;
-    image.src = item.familyphoto;
+    if (item.familyphoto) {
+      image.src = item.familyphoto;
+    
+      item.photobuttons.forEach(button => {
+        const itemelement = document.createElement('div');
+        itemelement.classList.add('family-button');
 
+        itemelement.style.top = button.top;
+        itemelement.style.left = button.left;
+        itemelement.style.width = button.width;
+        itemelement.style.height = button.height;
+
+        itemelement.addEventListener('click', () => {
+            let selectedFamilymember = familyMembers.find(member => member.name === button.name)
+            if (selectedFamilymember) {
+                navhistory.push(item)
+                 test(selectedFamilymember)
+            }
+        });
+        imgContainer.appendChild(itemelement);
+    });
+    } else {
+        image.src = "";
+    }
 }
 const urlParams = new URLSearchParams(window.location.search);
 const familyIndex = urlParams.get('Person')
 
 if (familyIndex !== null) {
-    const selectedFamilymember = familyMembers.find(member => member.name === familyIndex);
+    let selectedFamilymember = familyMembers.find(member => member.name === familyIndex);
     if (selectedFamilymember) {
         test(selectedFamilymember)
     } else {
         console.log("Family Member not found with name:", familyIndex);
     }
-}
+};
+
+const closeButton = document.getElementById('close-button');
+
+closeButton.addEventListener('click', () => {
+    if (navhistory.length > 0) {
+        const previousMember = navhistory.pop();
+        test(previousMember)
+    } else {
+        window.location.href = "../../index.html";
+    }
+});
