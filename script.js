2125d19d5608b3d0915480e48d17fd4ede9b5aef
LunaticStark <HyrimStark@gmail.com>
2026-08-25 12:00:23 -0400

    test

--- script.js
+++ script.js
@@ -60,3 +60,11 @@ document.querySelectorAll('.content-box').forEach(box => {
         });
     });
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+        .then((reg) => console.log('Service Worker Registered Successfully', reg.scope))
+        .catch((err) => console.error('Service Worker Registration Failed', err));
+    });
+}
---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'stanley-stories-v1';
+
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './style-mobile.css',
+    './script.js',
+    './manifest.json',
+    './Art-Stuff/full-family.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Server Worker: Caching App Shell');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) =>{
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Server Workers: Clearing Old Cache');
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
